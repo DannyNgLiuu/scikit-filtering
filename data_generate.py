62bc408d080792ac5ac34bcdf150e392fc0c5799
DannyNgLiuu <dannyngliu816@gmail.com>
2025-03-17 17:01:42 -0400

    implemented preference system

--- data_generate.py
+++ data_generate.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
 import random
-from datetime import datetime, timedelta
 
 np.random.seed(1)
 
-# Define sample values for categorical variables
 regions = ['NA', 'EU', 'ASIA', 'OCE', 'LATAM', 'AUS', 'MENA', 'KR']
 game_genres = ['FPS', 'MOBA', 'Battle Royale', 'MMO', 'RPG', 'Strategy', 'Sports', 'Racing', 'Card Game']
 game_modes = ['Competitive', 'Casual', 'Solo', 'Squad', 'Ranked', 'Custom', 'Tournament']
@@ -26,19 +24,20 @@ for i in range(0, num_rows):
     game_genre = random.choice(game_genres)
     game_mode = random.choice(game_modes)
     platform = random.choice(platforms)
-    toxicity_score = round(np.random.beta(1, 4), 2)
-    reports = np.random.poisson((toxicity_score * 15 + 1))
+    reputation = int(np.clip(np.random.normal(450, 200), -100, 1000))
+    
+    reports = np.random.poisson(max(1, (15 * (1 - (reputation + 100)/1100))))
     friend_overlap = np.random.binomial(30, 0.3)
     playstyle_tags = random.choice(playstyle_options)
     skill_level = random.choice(skill_levels)
     age_value = np.random.normal(25, 7)
-    age = int(np.clip(age_value, 13, 65)) 
+    age = int(np.clip(age_value, 13, 65))
 
     data.append({
         'Player ID': player_id,
         'Region': region,
         'Game Genre': game_genre,
-        'Toxicity Score': toxicity_score,
+        'Reputation': reputation,
         'Reports': reports,
         'Friend List Overlap': friend_overlap,
         'Preferred Game Mode': game_mode,
--- knn.py
+++ knn.py
@@ -4,24 +4,43 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import NearestNeighbors
 
-
 df = pd.read_csv("./nexus_data.csv", keep_default_na=False)
 
 categorical_features = ['Region', 'Game Genre', 'Preferred Game Mode', 'Platform', 'Playstyle Tags', 'Skill Level']
-numerical_features = ['Toxicity Score', 'Reports', 'Friend List Overlap', 'Age']
+numerical_features = ['Reputation', 'Reports', 'Friend List Overlap', 'Age']
 
-feature_weights = {
+default_weights = {
     'Region': 0.3,
     'Game Genre': 0.3,  
     'Preferred Game Mode': 0.05,  
     'Platform': 0.30,
     'Playstyle Tags': 0.05,  
     'Skill Level': 0.05,  
-    'Toxicity Score': 0.2,
+    'Reputation': 0.2,
     'Reports': 0.2,
     'Friend List Overlap': 0.05,  
     'Age': 0.1
 }
+
+#features with 0's will not be shown in the frontend for preferences
+preferences = {
+    'Region': False,
+    'Game Genre': False,
+    'Preferred Game Mode': False,
+    'Platform': False,
+    'Playstyle Tags': False,
+    'Skill Level': False,
+    'Reputation': 0,
+    'Reports': 0,
+    'Friend List Overlap': 0,
+    'Age': 0
+}
+
+for key in preferences:
+    if preferences[key] is True:
+        preferences[key] = 1
+    elif preferences[key] is False:
+        preferences[key] = 0
 
 preprocessor = ColumnTransformer(
     transformers=[
@@ -31,46 +50,61 @@ preprocessor = ColumnTransformer(
 
 X = preprocessor.fit_transform(df[numerical_features + categorical_features])
 
+#combines categorical features with their data ex. Region_NA, Region_EU, etc
 encoded_categorical_features = preprocessor.transformers_[1][1].get_feature_names_out(categorical_features).tolist()
 
+#combines numerical features with encoded_categorical_features
 all_feature_names = numerical_features + encoded_categorical_features
 
-weights = np.array(
-    [feature_weights[feature] for feature in numerical_features] + 
-    [feature_weights[feature.split('_')[0]] for feature in encoded_categorical_features]
+#applies the weights to the features
+dweights = np.array(
+    [default_weights[feature] for feature in numerical_features] + 
+    [default_weights[feature.split('_')[0]] for feature in encoded_categorical_features]
+)
+
+pweights = np.array(
+    [preferences[feature] for feature in numerical_features] + 
+    [preferences[feature.split('_')[0]] for feature in encoded_categorical_features]
 )
 
 #ensures it is an array so it can be properly weighted
 X = X.toarray()
-X_weighted = X * weights
+
+if any(preferences.values()):
+    X_weighted = X * pweights
+else:
+    X_weighted = X * dweights
+
 
 #plus 1 more because user x is not included
 knn = NearestNeighbors(n_neighbors=6, metric='cosine')
 knn.fit(X_weighted)
-
 
 def get_recommendations_ml(player_id, df, knn_model):
 
     player_data = X_weighted[player_id].reshape(1, -1)
 
     #finds similar players using kNN
-    distance, indices = knn_model.kneighbors(player_data)
+    _, indices = knn_model.kneighbors(player_data)
 
     #retrieve recommended players except itself
     similar_indices = indices.flatten()[1:]
 
     recommendations = pd.DataFrame({
         'Player ID': df.iloc[similar_indices]['Player ID'].values,
-
     })
 
     #gets each users information
     full_recommendations = pd.merge(recommendations, df, on='Player ID')
     
-    return full_recommendations[['Player ID', 'Region', 'Game Genre', 'Platform', 'Playstyle Tags', 
-                                 'Preferred Game Mode', 'Skill Level', 'Toxicity Score', 'Reports', 'Age']]
+    return full_recommendations[['Player ID', 'Region', 'Game Genre', 'Reputation', 'Reports', 
+                                 'Friend List Overlap', 'Preferred Game Mode', 'Platform', 'Playstyle Tags', 'Skill Level', 'Age']]
 
-example_player_id = 1
-recommendations = get_recommendations_ml(example_player_id, df, knn)
+user_id = 1
+recommendations = get_recommendations_ml(user_id, df, knn)
+
+print(df[df['Player ID'] == user_id].to_string(index=False))
+
 print(recommendations.to_string(index=False))
+
 
